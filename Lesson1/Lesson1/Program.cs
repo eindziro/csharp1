920ebeaca90295a34d39f82dc7cf210ec4651118
eindziro <eindziro@yandex.ru>
2019-02-13 21:00:12 +0300

    сделал первое свое задание (опять)

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -8,10 +8,36 @@ namespace Lesson1
 {
     class Program
     {
+
+        static void Anketa()
+        {
+            string name, surname;
+            int age;
+            Console.WriteLine("Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Surname:");
+            surname = Console.ReadLine();
+            Console.WriteLine("age:");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(name + " " + surname + " " +age);
+            Console.WriteLine($"{name} {surname} {age}");
+            Console.WriteLine("{0} {1} {2}", name, surname,age);
+
+            string format = String.Format("{0} {1} {2}", name, surname, age);
+            Console.WriteLine(format);
+
+            Console.ReadKey();
+
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Kiss my Shiny ass!!!!Buzinga!!!");
-            Console.ReadKey();
+            Anketa();
+
+           
+
+
         }
     }
 }
