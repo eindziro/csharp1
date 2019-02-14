bbeec708fb4483a1a3f861fbbbe4895eb146974a
eindziro <eindziro@yandex.ru>
2019-02-14 16:28:16 +0300

    начал делать t1(lesson 4)

---
+++ Lesson4/Lesson4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Program
+    {
+
+
+        #region T1
+        static void PairCount()
+        {
+
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 6, 2, 9, -3, 6 };
+            StaticClass.PairCountProc(arr);
+            Console.ReadKey();
+        }
+
+        static int[] ReadArr(string path)
+        {
+            string tmp;
+            System.IO.File.ReadAllLines(path);
+            int[] arr = {1,2 };
+            return arr;
+        }
+        
+    }
+}
---
+++ Lesson4/Lesson4/StaticClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+     class StaticClass
+     {
+        
+        
+
+        public static void PairCountProc(int[] arr)
+        {
+            int PairCount = 0; ;
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                PairCount += arr[i]%3 == 0 && arr[i+1] % 3!=0|| arr[i] % 3 != 0 && arr[i + 1] % 3 == 0 ? 1 : 0;     
+
+            }
+            Console.WriteLine(PairCount);
+
+        }
+
+
+    }
+}
